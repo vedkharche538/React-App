12daee4906823825a725375b42b6d4749073d63d
vedhas <vedhaskharche@gmail.com>
2023-01-21 23:54:08 +0530

    Added copy text logic and clearout whitespaces

--- my-react-app/src/components/Textform.js
+++ my-react-app/src/components/Textform.js
@@ -18,9 +18,23 @@ export default function TextForm(props) {
         // console.log("On change");
         setText(event.target.value)
     }
-    const handleClearClick = ()=>{ 
+    const handleClearClick = () => {
         let newText = '';
         setText(newText)
+    }
+    // Credits: A
+    const handleCopy = () => {
+        console.log("I am copy");
+        var text = document.getElementById("myBox");
+        text.select();
+        text.setSelectionRange(0, 9999);
+        navigator.clipboard.writeText(text.value);
+    }
+
+    // Credits: Coding Wala
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "))
     }
 
     const [text, setText] = useState('');
@@ -36,6 +50,8 @@ export default function TextForm(props) {
                 <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
                 <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
                 <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
+                <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
+                <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
             </div>
             <div className="container my-3">
                 <h2>Your text summary</h2>
