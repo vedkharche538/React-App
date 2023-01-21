10b15c712def005762e8b56bd1550b43c6f2a600
vedhas <vedhaskharche@gmail.com>
2023-01-21 23:08:15 +0530

    Clear button added

--- my-react-app/src/components/Textform.js
+++ my-react-app/src/components/Textform.js
@@ -18,6 +18,10 @@ export default function TextForm(props) {
         // console.log("On change");
         setText(event.target.value)
     }
+    const handleClearClick = ()=>{ 
+        let newText = '';
+        setText(newText)
+    }
 
     const [text, setText] = useState('');
     // text = "new text"; // Wrong way to change the state
@@ -31,6 +35,7 @@ export default function TextForm(props) {
                 </div>
                 <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
                 <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
+                <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
             </div>
             <div className="container my-3">
                 <h2>Your text summary</h2>
